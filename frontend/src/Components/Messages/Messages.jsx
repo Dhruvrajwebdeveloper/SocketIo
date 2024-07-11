c02c643d8b779af12c4355acb1eac8e45ec2203a
Dhruv Raj <dhruvcoolraj@gmail.com>
2024-07-11 21:01:31 +0530

    Completed Project

---
+++ frontend/src/Components/Messages/Messages.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Messages() {
+  return (
+    <div className='messages-container' style={{
+        height:"70%",  
+        width:"inherit",
+        
+    }}>
+      
+    </div>
+  )
+}
+ 
+export default Messages
